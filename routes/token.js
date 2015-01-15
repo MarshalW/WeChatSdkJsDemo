f2f58b1e00687f013ea5f6c893cbd1683911f547
Marshal <marshal.wu@qq.com>
2015-01-15 21:20:56 +0800

    changed, tocken can reuse

--- routes/token.js
+++ routes/token.js
@@ -25,17 +25,34 @@ router.get('/', function(req, res) {
 		  	var ticket=JSON.parse(body).ticket;
 		  	console.log('ticket: '+ticket);
 
-		  	var port = req.app.settings.port || cfg.port;
+		  	var port = req.app.settings.port;
 		  	var url=req.protocol + '://' + req.host  + ( port == 80 || port == 443 ? '' : ':'+port ) + req.path;
-		  	var result=sign(ticket,req.url);
+		  	var result=sign(ticket,url);
 
 		  	res.send(result);
 		});
 	});  
 });
 
+var globalToken={};
+var max_live=7200-1000;
+
 /* 正式的演示代码，包括服务器端生成签名，客户端通过签名使用微信API方法 */
 router.get('/client', function(req, res) {
+	var now=new Date().getTime()/1000;
+
+	if (!globalToken.time || now-globalToken.time>max_live){
+		refreshGlobalToken(req,res,function(){
+			console.log('获取新的token');
+			renderClient(req,res);
+		})
+	}else{
+		console.log('重复使用token');
+		renderClient(req,res);
+	}
+});	
+
+function refreshGlobalToken(req,res,callback){
 	request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+config.appId+'&secret='+config.appSecret, function (error, response, body) {
 		console.log('根据appId和appSecret获取accessToken ..');
 		if(error || response.statusCode != 200){
@@ -55,14 +72,18 @@ router.get('/client', function(req, res) {
 		  	var ticket=JSON.parse(body).ticket;
 		  	console.log('ticket: '+ticket);
 
-		  	var port = req.app.settings.port || cfg.port;
-		  	var url=req.protocol + '://' + req.host  + ( port == 80 || port == 443 ? '' : ':'+port ) + req.path;
-		  	var result=sign(ticket,url);
-
-		  	//这之前，都和上面的「router.get('/', function(req, res) {..」是一样的。
-		  	res.render('client',{config:config, result:result});
+		  	globalToken.time=new Date().getTime()/1000;
+		  	globalToken.ticket=ticket;
+		  	callback();
 		});
 	});  
-});	
+}
+
+function renderClient(req,res){
+	var port = req.app.settings.port;
+	var url=req.protocol + '://' + req.host  + ( port == 80 || port == 443 ? '' : ':'+port ) + req.path;
+	var result=sign(globalToken.ticket,url);
+	res.render('client',{config:config,result:result});
+}
 
 module.exports = router;
